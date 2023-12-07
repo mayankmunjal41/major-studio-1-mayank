1b835f254692485f93f8fe243779a276887f5c20
Mayank Munjal <mayankmunjal95@gmail.com>
2023-12-07 12:15:22 -0500

    added color swatch functionality

--- NGA-final/app.js
+++ NGA-final/app.js
@@ -1,5 +1,5 @@
 let height = window.innerHeight;
-let width = window.innerWidth;
+let width = 1400;
 let margin = { top: 50, right: 50, bottom: 50, left: 50 };
 
 let chartState = {};
@@ -7,14 +7,13 @@ let chartState = {};
 let svg = d3.select("#svganchor").append("svg").attr("width", width).attr("height", height);
 
 let xScale = d3.scaleLinear().range([margin.left, width - margin.right]);
-
 svg
   .append("g")
   .attr("class", "x axis")
   .attr("transform", "translate(0," + (height - margin.bottom) + ")");
 
 // Load and process data
-d3.csv("closest_color.csv")
+d3.csv("closest_color_rgb.csv")
   .then(function (data) {
     let dataSet = data;
     console.log(dataSet)
@@ -46,8 +45,8 @@ d3.csv("closest_color.csv")
       let simulation = d3.forceSimulation(dataSet)
         .force("x", d3.forceX(function (d) {
           return xScale(+d.beginyear);
-        }).strength(1))
-        .force("y", d3.forceY((height / 2) - margin.bottom / 2))
+        }).strength(10))
+        .force("y", d3.forceY((height / 2) + 20))
         .force("collide", d3.forceCollide(6))
         .stop();
 
@@ -66,47 +65,59 @@ d3.csv("closest_color.csv")
         .append("circle")
         .attr("class", "countries")
         .attr("cx", function (d) {
-          return xScale(d.beginyear);
+          return xScale(d.beginyear) + margin.left;
         })
         .attr("cy", (height / 2) - margin.bottom / 2)
         .attr("r", 5.5)
         .attr("fill", function (d) {
-          return `${d.hex_color}`; // Use the hex_color value directly from the dataset
+          return `rgb${d.hex_color}`;
         })
         .merge(countriesCircles)
         .on("click", function (event, d) {
-     
-          let firstSwatch = d3.select("#color-1")
+          d3.select('body').style('overflow','auto');
+          let firstSwatch = d3.select("#color-1");
           d3.select("#svganchor").style('display', 'none');
           d3.select("#palette-container").style("display", "block");
 
           // select color and change color for first swatch
           let selectedColor = this.getAttribute('fill');
           let swatchColor = firstSwatch.style('background-color', selectedColor);
-          // console.log(data)
-          const rgbToHex = (r, g, b) => {
-            return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-          }
-          console.log(selectedColor)
-          console.log(rgbToHex(selectedColor)); 
+
           d3.selectAll(".countries")
-          .each(function (d) {
-            if (d.hex_color === selectedColor) {
-             console.log('yes')
-            }
-            // console.log(d.hex_color)
-            // console.log(selectedColor)
-          });
+            .each(function (d) {
+              if (`rgb${d.hex_color}` === selectedColor) {
+                let imgContainer = d3.select('.images').append('div').attr('class', 'img');
+
+                imgContainer.append('img').attr('src', d.imageurl);
+
+                let swatchContainer = imgContainer.append('div').attr('class', 'swatch');
+                swatchContainer.append('div').attr('class', 'vibrant').style('background-color', d.Vibrant);
+                swatchContainer.append('div').attr('class', 'lightVibrant').style('background-color', d.LightVibrant);
+                swatchContainer.append('div').attr('class', 'darkVibrant').style('background-color', d.DarkVibrant);
+                swatchContainer.append('div').attr('class', 'muted').style('background-color', d.Muted);
+                swatchContainer.append('div').attr('class', 'lightMuted').style('background-color', d.LightMuted);
+
+                d3.selectAll('.swatch > div').on('click', function() {
+                  let selectedSwatch = getComputedStyle(this).backgroundColor;
+
+                  let firstEmptyColor = d3.selectAll('.color').filter(function() {
+                    let backgroundColor = d3.select(this).style('background-color');
+                    return backgroundColor === 'rgba(0, 0, 0, 0)' || backgroundColor === 'transparent';
+                  }).node();
+
+                  if (firstEmptyColor) {
+                    d3.select(firstEmptyColor).style('background-color', selectedSwatch);
+                  }
+                });
+              }
+            });
         })
         .on("mouseover", function (event, d) {
-          // Log the color of the hovered circle
-          let hoveredFillColor = d.hex_color;
-          console.log("Hovered Circle Fill Color:", hoveredFillColor);
+          let hoveredFillColor = `rgb${d.hex_color}`;
 
-          // Identify and change the color of circles that don't match the hovered circle's fill color
           d3.selectAll(".countries")
             .each(function (datum) {
-              if (datum.hex_color !== hoveredFillColor) {
+              if (`rgb${datum.hex_color}` !== hoveredFillColor) {
                 d3.select(this)
                   .transition()
                   .duration(200)
@@ -115,12 +126,11 @@ d3.csv("closest_color.csv")
             });
         })
         .on("mouseout", function (event, d) {
-          // Revert the color for all circles on mouseout
           d3.selectAll(".countries")
             .transition()
             .duration(200)
             .attr("fill", function (datum) {
-              return datum.hex_color;
+              return `rgb${datum.hex_color}`;
             });
         })
         .transition()
@@ -132,8 +142,31 @@ d3.csv("closest_color.csv")
           return d.y;
         })
         .attr("fill", function (d) {
-         return `${d.hex_color}`;
+          return `rgb${d.hex_color}`;
         });
+
+      // Append horizontal line
+      svg.append("line")
+        .attr("x1", margin.left - 20)
+        .attr("y1", (height) / 2 + 30)
+        .attr("x2", width + 1000)
+        .attr("y2", (height) / 2 + 30)
+        .attr("stroke", "white")
+        .attr("stroke-width", 0.5);
+
+      // Append vertical lines
+      svg.selectAll(".vertical-line")
+      // (width - margin.right) / 2, width - margin.right)
+        .data([margin.left, ])
+        .enter()
+        .append("line")
+        .attr("class", "vertical-line")
+        .attr("x1", function(d) { return d; })
+        .attr("y1", 200)
+        .attr("x2", function(d) { return d; })
+        .attr("y2", height - 50)
+        .attr("stroke", "white")
+        .attr("stroke-width", 0.5);
     }
   })
   .catch(function (error) {
